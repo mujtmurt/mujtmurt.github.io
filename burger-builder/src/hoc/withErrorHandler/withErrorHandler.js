d453a2f04437933b440163e01d8c603e11987e5f
Muj <mujtmurt@gmail.com>
2020-07-06 09:12:22 -0400

    burger builder

---
+++ burger-builder/src/hoc/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import useHttpErrorHandler from "../../hooks/http-error-handler";
+import Modal from "../../components/UI/Modal/Modal";
+import Aux from "../Aux/Aux";
+
+const withErrorHandler = (WrappedComponent, axios) => {
+  return (props) => {
+    const [error, errorConfirmedHandler] = useHttpErrorHandler(axios);
+
+    return (
+      <Aux>
+        <Modal show={error} modalClosed={errorConfirmedHandler}>
+          {error ? error.message : null}
+        </Modal>
+        <WrappedComponent {...props} />
+      </Aux>
+    );
+  };
+};
+
+export default withErrorHandler;
